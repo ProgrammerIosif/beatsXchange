66699b23bac78490a450b909ce7471a8a5693748
ProgrammerIosif <programmeriosif@protonmail.com>
2023-06-12 14:15:38 +0300

    reworked the prismaClient to solve the multiple 'Prisma Client Instances' error

--- app/(application)/shop/page.tsx
+++ app/(application)/shop/page.tsx
@@ -1,5 +1,5 @@
 import { Product } from '@prisma/client'
-import prisma from '@/prisma/client'
+import { prisma } from '@/prisma/client'
 import Link from 'next/link'
 
 async function getProducts() {
